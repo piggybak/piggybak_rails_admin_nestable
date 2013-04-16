d7bf3a2e2b91dfebed80043009771147ea9cb389
Tim Case <tim@endpoint.com>
2013-04-15 19:29:36 -0700

    ensure assets precompile

--- lib/rails_admin_nestable/engine.rb
+++ lib/rails_admin_nestable/engine.rb
@@ -1,4 +1,7 @@
 module RailsAdminNestable
   class Engine < ::Rails::Engine
+    initializer "piggybak_rails_admin_nestable.precompile_hook", :group => :all do |app|
+      app.config.assets.precompile += ['rails_admin/rails_admin_nestable.js']
+    end
   end
 end
